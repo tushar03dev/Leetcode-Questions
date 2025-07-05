b604ed73ebc631653f8a8ccaf0d5191850af0cc9
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-05 23:48:07 +0530

    Create jump.cpp

---
+++ Arrays/45. Jump Game II/jump.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int near = 0;
+        int far = 0;
+        int jumps = 0;
+
+        while(far < nums.size()-1){
+            int farthest = 0;
+            for(int i = near; i <= far; i++){
+                farthest = max(farthest,i+nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;
+    }
+};
