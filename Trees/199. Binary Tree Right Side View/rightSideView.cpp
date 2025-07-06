6630a57ef023f02ed35262260214367f7331084f
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-06 23:58:24 +0530

    Create rightSideView.cpp

---
+++ Trees/199. Binary Tree Right Side View/rightSideView.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, int level, vector<int>& ans){
+        if(!root) return;
+
+        if(level == ans.size()){
+            ans.push_back(root->val);
+        }
+
+        solve(root->right,level+1,ans);
+        solve(root->left,level+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ans;
+        int level  = 0;
+        solve(root,level,ans);
+        return ans;
+    }
+};
