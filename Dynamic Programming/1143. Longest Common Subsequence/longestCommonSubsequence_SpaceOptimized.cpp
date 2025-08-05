c07ec87d1747dac6fedcabb08e14d89f5e12281e
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-08-05 17:54:33 +0530

    Create longestCommonSubsequence_SpaceOptimized.cpp

---
+++ Dynamic Programming/1143. Longest Common Subsequence/longestCommonSubsequence_SpaceOptimized.cpp
@@ -0,0 +1,20 @@
+int longestCommonSubsequence(string text1, string text2) {
+        int i = 0, j = 0;
+        vector<int> next(text1.size() + 1, 0);
+        vector<int> curr(text2.size() + 1, 0);
+
+        for (int j = text2.size() - 1; j >= 0; j--) {
+            for (int i = text1.size() - 1; i >= 0; i--) {
+                int ans =0;
+                if (text1[i] == text2[j]) {
+                    ans = 1 + next[i + 1];
+                } else {
+
+                    ans = max(next[i],curr[i + 1]);
+                }
+                curr[i] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
