6f751579b42b62341dd411246cddf8bd8d5d3b71
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-08-21 23:47:51 +0530

    Create lengthOfLISBinarySearch.cpp

---
+++ Dynamic Programming/300. Longest Increasing Subsequence/lengthOfLISBinarySearch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ans;
+        // initial case
+        ans.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            } else{
+                int index = lower_bound(ans.begin(),ans.end(),nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+
+        return ans.size();
+    }
+};
